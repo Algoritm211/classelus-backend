abc1526e3079a6cf4bb248d54c00c33794cdf407
Alexey <algoritm211@gmail.com>
2022-08-31 21:23:35 +0300

    Modify cookie options v3

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,6 +8,8 @@ const cookieOptions = {
   expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
   maxAge: 24 * 60 * 60 * 1000,
   httpOnly: true,
+  path: '/',
+  domain: 'classelus.com',
   sameSite: 'lax',
 }
 
